5f525ae1bbef41a660327db5228726e127d33ee1
Shadownc <365172043@qq.com>
2024-09-13 01:50:39 +0000

    新增站点

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,Voicemail,Images,Markdown,CompressAlt,RecordVinyl } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,Voicemail,Images,Markdown,CompressAlt,RecordVinyl,Tools } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -74,7 +74,8 @@ const siteIcon = {
   Images,
   Markdown,
   CompressAlt,
-  RecordVinyl
+  RecordVinyl,
+  Tools
 };
 
 // 链接跳转
